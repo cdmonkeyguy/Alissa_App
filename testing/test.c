b7ac50a01ebc6bf7a3bd7b896f5830bcbc50d222
corbinmgraham <corbin@metagraham.com>
2022-03-07 21:54:23 -0600

    Moving to V1

--- testing/test.c
+++ testing/test.c
@@ -22,7 +22,7 @@
 
 // Macros
 #define URL "https://raw.githubusercontent.com/cdmonkeyguy/Alissa_App/main/install/alissa.c"
-#define VERSION 0.114   // version sub_version release patch
+#define VERSION 0.121   // version sub_version release patch
 
 int app();
 int auto_update();
